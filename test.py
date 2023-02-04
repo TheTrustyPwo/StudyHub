a159da686e5c6f4b7eb4e12b4a7b26d62626fef1
TheTrustyPwo <shucecai@gmail.com>
2023-02-04 23:19:28 +0800

    Create test cases and testing script

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+def main():
+    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/base.py
@@ -0,0 +1,47 @@
+from app import app, db
+from flask_testing import TestCase
+import json
+
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        """
+        Create an instance of the app with the testing configuration
+        :return:
+        """
+        app.config.from_object('app.config.TestingConfig')
+        return app
+
+    def setUp(self):
+        """
+        Create the database
+        :return:
+        """
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        """
+        Drop the database tables and also remove the session
+        :return:
+        """
+        db.session.remove()
+        db.drop_all()
+
+    def register_user(self, email, password):
+        """
+        Helper method for registering a user with dummy data
+        :return:
+        """
+        return self.client.post(
+            'v1/auth/register',
+            content_type='application/json',
+            data=json.dumps(dict(email=email, password=password)))
+
+    def get_user_token(self):
+        """
+        Get a user token
+        :return:
+        """
+        auth_res = self.register_user('example@gmail.com', '12345678')
+        return json.loads(auth_res.data.decode())['auth_token']
