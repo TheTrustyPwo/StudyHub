eecc18f28dc5825e1b242240053f6f67ef8e7802
TheTrustyPwo <shucecai@gmail.com>
2023-02-04 23:19:09 +0800

    Setup Application

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 import os
+
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 # Initialize application
@@ -8,6 +11,25 @@ app = Flask(__name__, static_folder=None)
 # Enabling cors
 CORS(app)
 
+# app configuration
+app_settings = os.getenv('APP_SETTINGS', 'app.config.DevelopmentConfig')
+app.config.from_object(app_settings)
+
+# Initialize Bcrypt
+bcrypt = Bcrypt(app)
+
+# Initialize Flask Sql Alchemy
+db = SQLAlchemy(app)
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+from app.users.auth import auth
+
+app.register_blueprint(auth, url_prefix='/v1')
+
+with app.app_context():
+    db.create_all()
