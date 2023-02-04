148ce8b69c07e5b85c64e7c051a302c31feb2d83
TheTrustyPwo <shucecai@gmail.com>
2023-02-04 23:17:38 +0800

    Add config for Dev & Testing

---
+++ app/config.py
@@ -0,0 +1,39 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    """
+    Base application configuration
+    """
+    DEBUG = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'strong_key')
+    BCRYPT_HASH_PREFIX = 14
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AUTH_TOKEN_EXPIRY_DAYS = 30
+    AUTH_TOKEN_EXPIRY_SECONDS = 3000
+    JWT_SIGNATURE_ALGORITHM = 'HS256'
+
+
+class DevelopmentConfig(BaseConfig):
+    """
+    Development application configuration
+    """
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    BCRYPT_HASH_PREFIX = 4
+    AUTH_TOKEN_EXPIRY_DAYS = 1
+    AUTH_TOKEN_EXPIRY_SECONDS = 20
+
+
+class TestingConfig(BaseConfig):
+    """
+    Testing application configuration
+    """
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL_TEST')
+    BCRYPT_HASH_PREFIX = 4
+    AUTH_TOKEN_EXPIRY_DAYS = 0
+    AUTH_TOKEN_EXPIRY_SECONDS = 3
+    AUTH_TOKEN_EXPIRATION_TIME_DURING_TESTS = 5
