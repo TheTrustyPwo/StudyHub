2a66dfba47c7e2b8b62b2c3d8a05c432a5d9c010
TheTrustyPwo <shucecai@gmail.com>
2023-03-29 10:49:52 +0800

    Create post model

---
+++ app/models/post.py
@@ -0,0 +1,41 @@
+import datetime
+from app import db
+
+
+class Post(db.Model):
+    """
+    Model that represents a post
+    """
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    post = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<Post (id='{self.id}', title='{self.title}', post='{self.post}', date_created='{self.date_created}')>"
+
+    def __init__(self, title: str, post: str, user_id: int):
+        self.title = title
+        self.post = post
+        self.user_id = user_id
+
+    def save(self):
+        """
+        Persist the post in the database
+        :return
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_by_id(post_id):
+        """
+        Filter a post by id
+        :param post_id
+        :return: User or None
+        """
+        return Post.query.filter_by(id=post_id).first()
+
