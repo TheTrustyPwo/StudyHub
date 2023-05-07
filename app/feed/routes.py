cce65851207e248f51d9f40d95bca473266b6d6c
TheTrustyPwo <shucecai@gmail.com>
2023-05-07 14:04:59 +0800

    Fixes

--- app/feed/routes.py
+++ app/feed/routes.py
@@ -3,7 +3,6 @@ from flask_login import current_user, login_required
 
 from app.feed import feed_blueprint, feed_service
 
-@login_required
 @feed_blueprint.route("/")
 @feed_blueprint.route("/home")
 def home():
