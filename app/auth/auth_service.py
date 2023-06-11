e7fb6af3ac956fb05f29eff3342738b92853a5cb
TheTrustyPwo <shucecai@gmail.com>
2023-06-11 19:12:20 +0800

    Annotate methods in auth service

--- app/auth/auth_service.py
+++ app/auth/auth_service.py
@@ -7,6 +7,13 @@ from app.models import User
 def register_user(email: str, username: str, password: str):
     """
     Hashes the given password and registers a new user in the database.
+
+    :param email: The email of the user.
+    :type email: str
+    :param username: The username of the user.
+    :type username: str
+    :param password: The password of the user.
+    :type password: str
     """
     hashed_password = bcrypt.hash(password)
     user = User(email=email.lower(), username=username, password=hashed_password)
@@ -17,13 +24,19 @@ def log_in_user(email, password):
     """
     Hashes and compares the given password with the stored password. If it is a match,
     logs a user in.
+
+    :param email: The email of the user.
+    :type email: str
+    :param password: The password of the user.
+    :type password: str
+    :return: True if the login is successful, False otherwise.
+    :rtype: bool
     """
     user = User.get_by_email(email.lower())
     if user and bcrypt.verify(password, user.password):
         login_user(user)
         return True
-    else:
-        return False
+    return False
 
 
 def log_out_user():
