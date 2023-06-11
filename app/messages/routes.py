13d826e29d49924f98a66d0ae13ae411c589895f
TheTrustyPwo <shucecai@gmail.com>
2023-06-11 19:15:35 +0800

    Update new message socket

--- app/messages/routes.py
+++ app/messages/routes.py
@@ -3,37 +3,15 @@ from flask_login import current_user, login_required
 from flask_socketio import emit, join_room, leave_room
 
 from app import socketio
+from app.conversations import conversation_services
 from app.messages import messages_blueprint, messages_service
-from app.models import User
+from app.models import User, Conversation
 
 
 @messages_blueprint.route("/messages")
 @login_required
 def messages():
-    chats = messages_service.get_chats(current_user.id)
-    return render_template("messages.html", chats=chats)
-
-
-@messages_blueprint.route("/messages/history")
-@login_required
-def messages_history():
-    target_id = request.args.get('user')
-    if target_id and target_id.isdigit():
-        chat_messages = messages_service.get_chat_messages(current_user.id, int(target_id))
-
-        serialized_messages = []
-        for message in chat_messages:
-            serialized_messages.append({
-                'id': message.id,
-                'sender': message.sender.username,
-                'recipient': message.recipient.username,
-                'content': message.content,
-                'timestamp': message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
-            })
-
-        return jsonify({'messages': serialized_messages})
-
-    return redirect(url_for("messages.messages"))
+    return render_template("messages.html")
 
 
 @socketio.on('connect', namespace="/messages/socket")
@@ -59,19 +37,11 @@ def handle_disconnect():
 @socketio.on('message', namespace="/messages/socket")
 @login_required
 def handle_message(payload):
-    recipient_id = int(payload['recipient_id'])
+    conversation_id = payload['conversation_id']
     content = payload['content']
 
-    message = messages_service.create_message(current_user.id, recipient_id, content)
-    response_data = {
-        "id": message.id,
-        "sender": current_user.username,
-        "sender_id": current_user.id,
-        "recipient": User.get_by_id(message.recipient_id).username,
-        "recipient_id": message.recipient_id,
-        "content": message.content,
-        "timestamp": message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
-    }
+    message = messages_service.create_message(current_user.id, conversation_id, content)
+    conversation = Conversation.get_by_id(conversation_id)
 
-    emit('new_message', response_data, room=current_user.id, json=True)
-    emit('new_message', response_data, room=recipient_id, json=True)
+    for user in conversation.users:
+        emit('new_message', message.serialized, room=user.id, json=True)
