a3ad494da0c03c9f9e5a3353ec1587f47a5725a1
TheTrustyPwo <shucecai@gmail.com>
2023-06-11 19:26:58 +0800

    Register new blueprints and error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from typing import Type
 from flask import Flask
 from flask_cors import CORS
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO, emit, Namespace
 from flask_moment import Moment
+from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
 from app.config import BaseConfig, DevelopmentConfig
 
@@ -38,12 +38,21 @@ def create_app(config: Type[BaseConfig] = DevelopmentConfig):
     from app.feed import feed_blueprint
     from app.replies import reply_blueprint
     from app.messages import messages_blueprint
+    from app.conversations import conversations_blueprint, conversations_api_blueprint
+    from app.users import user_blueprint, user_api_blueprint
 
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(post_blueprint)
     app.register_blueprint(feed_blueprint)
     app.register_blueprint(reply_blueprint)
     app.register_blueprint(messages_blueprint)
+    app.register_blueprint(conversations_blueprint)
+    app.register_blueprint(conversations_api_blueprint)
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(user_api_blueprint)
+
+    from app.exceptions.api_exception import APIException, handle_api_exception
+    app.register_error_handler(APIException, handle_api_exception)
 
     with app.app_context():
         db.create_all()
