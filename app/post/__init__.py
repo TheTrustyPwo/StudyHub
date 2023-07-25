4fef44bcb76e68f6e6e2d4cbca45d6ab77674d2a
TheTrustyPwo <shucecai@gmail.com>
2023-07-25 19:18:02 +0800

    Minor changes

--- app/post/__init__.py
+++ app/post/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
 post_blueprint = Blueprint("post", __name__)
+post_api_blueprint = Blueprint('post_api', __name__, url_prefix='/api/v1/posts')
 
-from app.post import routes
+from app.post import routes, api
