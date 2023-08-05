1ca550f919e5b2da5a6f158b507c107133ebc9e9
Stucknight <chengweifan2020@gmail.com>
2023-08-05 18:10:10 +0800

    added upload button, functionality and minor changes

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -1,20 +1,58 @@
 import { User, Conversation } from "./models/index.js";
 
+
 $(document).ready(async function () {
     const username = window.location.pathname.split("/").pop();
     const user = await User.getByUsername(username);
+    const date = new Date(user.dateCreated.toLocaleString());
     document.getElementById('username').innerText = user.username;
-    document.getElementById('datecreated').innerText = user.dateCreated;
+    document.getElementById('datecreated').innerText = date.toDateString();
     document.getElementById('email').innerText = user.email;
+    document.getElementById('pfp').innerHTML = user.pfp;
     const current = await User.getCurrent();
     const message = document.getElementById('message');
-    message.style.visibility = 'hidden';
+    const upload = document.getElementById('displayfile');
+
     if (current.id !== user.id) {
         message.style.visibility = 'visible';
+    } else {
+        upload.style.visibility = 'visible';
     }
 
     message.onclick = async () => {
         await Conversation.createPrivate(user.id);
         window.location.pathname = 'messages';
     };
+
+
+});
+
+async function uploadFile(file) {
+    const formData = new FormData();
+
+    formData.append('file', file);
+
+    const response = await fetch('/api/v1/users/pfp/upload', {
+        method: 'POST',
+        body: formData,
+        purpose: "PROFILE_PICTURE"
+    });
+
+    if (response.ok) {
+        console.log('File uploaded successfully:', response);
+    } else {
+        console.error('Error uploading file:', response);
+    }
+}
+
+async function refresh(){
+    location.reload();
+}
+document.getElementById('upload-btn').addEventListener('change', async () => {
+    const fileInput = document.getElementById('upload');
+    const file = fileInput.files[0];
+    if (file) {
+        await uploadFile(file);
+    }
+    await refresh()
 });
