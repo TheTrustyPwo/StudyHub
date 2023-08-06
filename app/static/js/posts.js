55c4361a75406d50bc8f76ad82c1dfbe0a9195bd
TheTrustyPwo <shucecai@gmail.com>
2023-08-06 22:31:19 +0800

    Update posts.js for new post page and loading screen

--- app/static/js/posts.js
+++ app/static/js/posts.js
@@ -1,121 +1,140 @@
-import {Post, Reply, User} from "./models/index.js";
+import { Post, Reply, User } from "./models/index.js";
+import { hideLoadingScreen } from "./index.js";
+
+const currentUser = await User.getCurrent();
+const searchParams = new URLSearchParams(window.location.search);
+const sortBy = searchParams.get('sortBy')?.toLowerCase() || 'recent';
+const postId = parseInt(window.location.pathname.split('/').pop());
 
 $(document).ready(async function () {
-    const currentUser = await User.getCurrent();
-    const searchParams = new URLSearchParams(window.location.search);
-    const sortBy = searchParams.get('sortBy')?.toLowerCase() || 'recent';
-
-    const postId = parseInt(window.location.pathname.split('/').pop());
     const post = await Post.getById(postId);
     await post.loadReplies();
+    renderPost(post);
+    renderReplies(post.replies);
 
-    const postTitle = document.getElementById('post-title');
-    const postBody = document.getElementById('post-body');
-    const postVotes = document.getElementById('post-votes');
-    const postTimestamp = document.getElementById('post-timestamp');
-    const postAuthor = document.getElementById('post-author');
-    const postReplies = document.getElementById('post-replies');
+    hideLoadingScreen();
 
-    const postUpvoteButton = document.getElementById('post-upvote-button');
-    const postDownvoteButton = document.getElementById('post-downvote-button');
-    const postDeleteButton = document.getElementById('post-delete')
+    const relatedPosts = await Post.getPosts(1, 10, null, new Set([post.subject]));
+    renderRelatedPosts(relatedPosts.filter(relatedPost => relatedPost.id !== post.id));
+});
 
-    const replyText = document.getElementById('reply-text');
-    const replyButton = document.getElementById('reply-button');
+function renderPost(post) {
+    const postUpvoteButton = document.getElementById('post-upvote');
+    const postDownvoteButton = document.getElementById('post-downvote');
+    const userVote = post.votes.find(vote => vote.userId === currentUser.id)?.vote || 0;
 
-    postUpvoteButton.onclick = async () => {
+    postUpvoteButton.classList.remove('active');
+    postDownvoteButton.classList.remove('active');
+
+    if (userVote === 1) postUpvoteButton.classList.add('active');
+    else if (userVote === -1) postDownvoteButton.classList.add('active');
+
+    document.getElementById('post-title').innerText = post.title;
+    document.getElementById('post-body').innerText = post.body;
+    document.getElementById('post-votes').innerText = post.getVoteCount().toLocaleString();
+    document.getElementById('post-reply-count').innerText = post.replyCount.toLocaleString();
+    document.getElementById('post-timestamp').innerText = post.timestamp.local().calendar();
+
+    document.getElementById('post-author').innerText = post.author.username;
+    document.getElementById('post-author-pfp').src = post.author.pfp;
+
+    if (post.attachment) {
+        document.getElementById('post-attachment-wrap').style.display = 'flex';
+        document.getElementById('post-attachment').src = post.attachment;
+    }
+
+    document.getElementById('post-upvote').onclick = async () => {
         await post.upvote();
-        await renderPost();
+        await renderPost(post);
     }
 
-    postDownvoteButton.onclick = async () => {
+    document.getElementById('post-downvote').onclick = async () => {
         await post.downvote();
-        await renderPost();
+        await renderPost(post);
     }
 
-    postDeleteButton.onclick = async () => {
-        if (window.confirm('Are you sure you want to delete this post?')) {
-            await post.delete();
-            window.location.href = '/';
-        }
-    }
+    // postDeleteButton.onclick = async () => {
+    //     if (window.confirm('Are you sure you want to delete this post?')) {
+    //         await post.delete();
+    //         window.location.href = '/';
+    //     }
+    // }
 
-    await renderPost();
-    await renderReplies();
-
-    replyButton.onclick = async () => {
+    document.getElementById('reply-button').onclick = async () => {
+        const replyText = document.getElementById('reply-text');
         const text = replyText.value;
         replyText.value = '';
+
+        document.getElementById('reply-button').classList.add('d-none');
+        document.getElementById('reply-button-loading').classList.remove('d-none');
         const reply = await Reply.create(text, post);
-        const element = await renderReply(reply);
-        postReplies.prepend(element);
-        element.scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});
+        document.getElementById('reply-button-loading').classList.add('d-none');
+        document.getElementById('reply-button').classList.remove('d-none');
+
+        const replyDiv = renderReply(reply);
+        document.getElementById('post-replies').prepend(replyDiv);
+        replyDiv.scrollIntoView({behavior: 'auto', block: 'center', inline: 'center'});
+    }
+}
+
+function renderReplies(replies) {
+    const postReplies = document.getElementById('post-replies');
+    const sorting = {
+        'recent': (a, b) => b.timestamp - a.timestamp,
+        'popular': (a, b) => b.getVoteCount() - a.getVoteCount() || b.timestamp - a.timestamp
+    }
+    replies.sort(sorting[sortBy]).forEach(reply => postReplies.appendChild(renderReply(reply)));
+}
+
+function renderReply(reply) {
+    const userVote = reply.votes.find(vote => vote.userId === currentUser.id)?.vote || 0;
+    console.log(userVote)
+    const replyDiv = document.createElement('div');
+    replyDiv.setAttribute('data-reply-id', reply.id);
+    replyDiv.classList = 'card post-card w-100 shadow-xss rounded-xxl border-0 p-4 mb-3'
+
+    replyDiv.innerHTML =
+        `<div class="card-body p-0 d-flex">
+            <figure class="avatar me-3"><img src=${reply.author.pfp} alt="avater" class="shadow-sm rounded-circle w45"></figure>
+            <div><h4 class="fw-700 text-grey-900 font-xssss mt-1">${reply.author.username}<span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500">${reply.timestamp.fromNow()}</span></h4></div>
+        </div>
+        <h4 class="mont-font fw-600 font-xss text-dark">${reply.reply}</h4>
+        <div class="card-body d-flex p-0">
+            <div class="d-flex align-items-center fw-600 text-grey-900 text-dark lh-26 font-xssss me-2">
+                <i class="upvote ${userVote === 1 ? 'active' : ''}"></i>
+                <span id="post-votes" class="mx-2">${reply.getVoteCount().toLocaleString()}</span>
+                <i class="downvote ${userVote === -1 ? 'active' : ''}"></i>
+            </div>
+        </div>`;
+
+    replyDiv.querySelector('.upvote').onclick = async () => {
+        await reply.upvote();
+        replyDiv.replaceWith(renderReply(reply));
     }
 
-    async function renderPost() {
-        const userVote = post.votes.find(vote => vote.userId === currentUser.id)?.vote || 0;
-        postUpvoteButton.classList.remove('active');
-        postDownvoteButton.classList.remove('active');
-        if (userVote === 1) postUpvoteButton.classList.add('active');
-        else if (userVote === -1) postDownvoteButton.classList.add('active');
-
-        postTitle.innerText = post.title;
-        postBody.innerText = post.body;
-        postVotes.innerText = post.getVoteCount().toLocaleString();
-        postTimestamp.innerText = post.timestamp.local().calendar();
-        postAuthor.innerText = post.author.username;
+    replyDiv.querySelector('.downvote').onclick = async () => {
+        await reply.downvote();
+        replyDiv.replaceWith(renderReply(reply));
     }
 
-    async function renderReplies() {
-        const sorting = {
-            'recent': (a, b) => b.timestamp - a.timestamp,
-            'popular': (a, b) => b.getVoteCount() - a.getVoteCount() || b.timestamp - a.timestamp
-        }
-        post.replies.sort(sorting[sortBy]).forEach(reply => postReplies.appendChild(renderReply(reply)));
-    }
+    return replyDiv;
+}
 
-    function renderReply(reply) {
-        const userVote = reply.votes.find(vote => vote.userId === currentUser.id)?.vote || 0;
-        const element = document.createElement('div');
-        element.id = reply.id;
-        element.setAttribute('data-reply-id', reply.id);
-        element.classList.add('card', 'mb-2');
-
-        element.innerHTML =
-            `<div class="row">
-                <div class="col-2">
-                    <div class="list-group text-center border-0">
-                        <span></span>
-                        <div class="upvote-button ${userVote === 1 ? 'active' : ''}"></div>
-                        <span></span>
-                        <span class="votes1" id="votes1-1">${reply.getVoteCount().toLocaleString()}</span>
-                        <div class="downvote-button ${userVote === -1 ? 'active' : ''}"></div>
-                        <span></span>
-                    </div>
+function renderRelatedPosts(posts) {
+    const relatedPosts = document.getElementById('related-posts');
+    posts.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.innerHTML =
+            `<a href="/post/${post.id}" class="list-group-item list-group-item-action card w-100 border-0 rounded-0 pt-1">
+                <div class="card-body p-0 d-flex">
+                    <figure class="avatar me-1"><img src="${post.author.pfp}" alt="avater" class="shadow-sm rounded-circle w25"></figure>
+                    <h3 class="fw-600 text-grey-900 font-xsss lh-28">${post.author.username}</h3>
+                    <span class="fw-900 font-x mx-1">&#183;</span>
+                    <span class="font-xsss fw-500 text-grey-500">${post.timestamp.fromNow()}</span>
                 </div>
-                <div class="col-10">
-                    <div class="card-body">
-                        <div class="comment">
-                            <div class="comment-header">
-                                <span class="comment-author">${reply.author.username}</span>
-                                <span class="comment-time text-muted">${reply.timestamp.local().calendar()}</span>
-                            </div>
-                            <div class="comment-body"><p>${reply.reply}</p></div>
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-
-        element.querySelector('.upvote-button').onclick = async () => {
-            await reply.upvote();
-            element.replaceWith(renderReply(reply));
-        }
-
-        element.querySelector('.downvote-button').onclick = async () => {
-            await reply.downvote();
-            element.replaceWith(renderReply(reply));
-        }
-
-        return element;
-    }
-});
+                <h4 class="fw-bold font-xsss mt--1">${post.title}</h4>
+            </a>
+            <hr class="p-0 my-2">`;
+        relatedPosts.appendChild(postDiv);
+    })
+}
