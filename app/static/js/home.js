8c755721aaba81a0dcf59156bd9053385495583e
TheTrustyPwo <shucecai@gmail.com>
2023-08-06 22:36:51 +0800

    Filter posts by subject and lazy post loading

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,65 +1,165 @@
-import { User } from "./models/index.js";
+import { Post } from "./models/index.js";
+import { hideLoadingScreen } from "./index.js";
 
-let post_count
-const currentUrl = window.location.href;
+const subjects = [
+    {name: 'All Subjects', icon: 'globe'},
+    {name: 'English', icon: 'pen'},
+    {name: 'Mathematics', icon: 'calculator'},
+    {name: 'Physics', icon: 'lightning'},
+    {name: 'Chemistry', icon: 'rocket-takeoff'},
+    {name: 'Geography', icon: 'map'},
+    {name: 'History', icon: 'book'},
+    {name: 'Social Studies', icon: 'people'},
+    {name: 'Biology', icon: 'lungs'},
+    {name: 'Chinese', icon: 'translate'},
+    {name: 'Computing', icon: 'code-slash'},
+    {name: 'Literature', icon: 'journal'},
+    {name: 'Music', icon: 'music-note-beamed'},
+    {name: 'Art', icon: 'palette'}
+];
+
+const selectedSubjects = new Set();
+selectedSubjects.add('all subjects');
+
+let currentPage = 1;
+const postsPerPage = 2;
+let loading = false, reachedEnd = false;
+let lastPostTimestamp = null;
+
+window.addEventListener('scroll', handleScroll);
+
 $(document).ready(async function () {
-    post_count = parseInt(await (await fetch(`/api/v1/posts/count`)).json());
+    createSubjectButtons();
+    await fetchPosts();
+    hideLoadingScreen();
 });
 
-async function fetchPosts(from, to) {
-    return await fetch(`/api/v1/posts/${from}/${to}`)
+async function fetchPosts() {
+    loading = true;
+    document.getElementById('loading-spinner').classList.remove('d-none');
+
+    const posts = await Post.getPosts(currentPage, postsPerPage, lastPostTimestamp, selectedSubjects);
+    const postsContainer = document.getElementById('posts-container');
+
+    if (posts.length > 0) {
+        lastPostTimestamp = posts[posts.length - 1].timestamp;
+        posts.forEach(post => {
+            const postElement = createPostCard(post);
+            postsContainer.appendChild(postElement);
+        });
+    } else reachedEnd = true;
+
+    loading = false;
+    document.getElementById('loading-spinner').classList.add('d-none');
 }
 
-async function addPosts(posts) {
-    for (const post of posts) {
-        document.getElementById("posts").innerHTML += `<div class="col-12 col-md-6">
-                <div class="position-relative rounded-4 shadow-4-hover bg-surface-secondary">
-                    <div class="p-5 p-md-5 p-xl-10">
-                        <section>
-                            <header>
-                                <h1 class="h3 ls-tight mb-8">
-                                    ${post.ptitle}
-                                </h1>
-                            </header>
-                            <p class="text-muted mb-7">
-                                ${post.post}
-                            </p>
-                            <p class="text-muted mb-3">
-                                By ${(await User.getById(post.user)).username}
-                            </p>
-                            <footer>
-                                <a href=post/${post.id} class="font-semibold link-primary stretched-link">See Post -></a>
-                            </footer>
-                        </section>
-                    </div>
+function createPostCard(post) {
+    const postCard = document.createElement('div');
+    postCard.className = `card post-card w-100 shadow-xss rounded-xxl border-0 p-4 mb-3`;
+    postCard.innerHTML = `
+        <div class="card-body p-0 d-flex">
+            <figure class="avatar me-3"><img src=${post.author.pfp} alt="avater" class="shadow-sm rounded-circle w45"></figure>
+            <div><h4 class="fw-700 text-grey-900 font-xssss mt-1"> ${post.author.username} <span class="d-block font-xssss fw-500 mt-1 lh-3 text-grey-500"> ${post.timestamp.fromNow()}</span></h4></div>
+            <div class="ms-auto pointer"><i class="ti-more-alt text-grey-900 btn-round-md bg-greylight font-xss"></i></div>
+        </div>
+    
+        <h4 class="fw-bold font-xs">${post.title}</h4>
+        <div class="card-body p-0 me-lg-5">
+            <p class="fw-500 text-grey-500 lh-26 font-xssss w-100 mb-2">
+                ${post.body.length > 150 ? `${post.body.slice(0, 150)} <a href="/post/${post.id}" class="fw-600 text-primary ms-2">See more</a>` : post.body}
+            </p>
+        </div>
+    
+        ${post.attachment ? `
+        <div class="card-body d-block p-0 mb-3">
+            <div class="row ps-2 pe-2">
+                <div class="col-sm-12 p-1"><img src=${post.attachment} class="rounded-3 w-100" alt="post"></div>
+            </div>
+        </div>` : ''
+        }
+    
+        <div class="card-body d-flex p-0">
+            <div class="emoji-bttn pointer d-flex align-items-center fw-600 text-grey-900 text-dark lh-26 font-xssss me-2">
+                <i class="feather-thumbs-up text-white bg-primary-gradiant me-1 btn-round-xs font-xss"></i>
+                <i class="feather-heart text-white bg-red-gradiant me-2 btn-round-xs font-xss"></i>
+                ${post.getVoteCount()} Upvotes
+            </div>
+    
+            <a href="/post/${post.id}" class="d-flex align-items-center fw-600 text-grey-900 text-dark lh-26 font-xssss">
+                <i class="feather-message-circle text-dark text-grey-900 btn-round-sm font-lg"></i>
+                <span class="d-none-xss">${post.replyCount} Replies</span>
+            </a>
+    
+            <div class="pointer ms-auto d-flex align-items-center fw-600 text-grey-900 text-dark lh-26 font-xssss" id="dropdownMenu32" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="feather-share-2 text-grey-900 text-dark btn-round-sm font-lg"></i>
+                <span class="d-none-xs">Share</span>
+            </div>
+    
+            <div class="dropdown-menu dropdown-menu-end p-4 rounded-xxl border-0 shadow-lg right-0 " aria-labelledby="dropdownMenu32">
+                <h4 class="fw-700 font-xss text-grey-900 d-flex align-items-center">Share <i class="feather-x ms-auto font-xssss btn-round-xs bg-greylight text-grey-900 me-2"></i></h4>
+                <div class="card-body p-0 d-flex">
+                    <ul class="d-flex align-items-center justify-content-between mt-2">
+                        <li class="me-1"><span class="btn-round-lg pointer bg-tumblr"><i class="font-xs ti-tumblr text-white"></i></span></li>
+                        <li class="me-1"><span class="btn-round-lg pointer bg-youtube"><i class="font-xs ti-youtube text-white"></i></span></li>
+                        <li class="me-1"><span class="btn-round-lg pointer bg-flicker"><i class="font-xs ti-flickr text-white"></i></span></li>
+                        <li class="me-1"><span class="btn-round-lg pointer bg-black"><i class="font-xs ti-vimeo-alt text-white"></i></span></li>
+                        <li><span class="btn-round-lg pointer bg-whatsup"><i class="font-xs feather-phone text-white"></i></span></li>
+                    </ul>
                 </div>
-            </div>`
-    }
-}
-let startPostIndex = 0;
-const postsPerPage = 10;
+                <h4 class="fw-700 font-xssss mt-4 text-grey-500 d-flex align-items-center mb-3">Copy Link</h4>
+                <i class="feather-copy position-absolute right-35 mt-3 font-xs text-grey-500"></i>
+                <input type="text" placeholder="https://studyhub.thepwo.com/post/${post.id}" class="bg-grey text-grey-500 font-xssss border-0 lh-32 p-2 font-xssss fw-600 rounded-3 w-100 theme-dark-bg">
+            </div>
+        </div>`
 
-function isScrollAtBottom() {
-    const scrollOffset = window.innerHeight + window.scrollY;
-    const pageHeight = document.documentElement.scrollHeight;
-    return scrollOffset >= pageHeight;
+    postCard.onclick = () => window.location.href = `/post/${post.id}`;
+    return postCard;
 }
 
 async function handleScroll() {
-    if (isScrollAtBottom() && post_count > startPostIndex) {
-        const endPostIndex = startPostIndex + postsPerPage;
-        const newPosts = await(await fetchPosts(startPostIndex, endPostIndex)).json();
-
-        if (newPosts.length > 0) {
-            await addPosts(newPosts);
-            startPostIndex = endPostIndex;
-        }
+    const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+    if (!loading && !reachedEnd && scrollTop + clientHeight >= scrollHeight - 200) {
+        currentPage += 1;
+        await fetchPosts();
     }
 }
 
-window.addEventListener('scroll', handleScroll);
-window.addEventListener('load', async () => {
-    const initialPosts = await (await fetchPosts(startPostIndex, startPostIndex + postsPerPage)).json();
-    await addPosts(initialPosts);
-    startPostIndex += postsPerPage;
-});
+function createSubjectButtons() {
+    const subjectButtonsContainer = document.getElementById('subject-buttons');
+    subjectButtonsContainer.innerHTML = '';
+
+    subjects.forEach(subject => {
+        const buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('d-flex', 'flex-row', 'align-items-start');
+
+        const button = document.createElement('button');
+        button.type = 'button';
+        button.classList.add('btn', 'font-weight-bold', 'me-2', 'mb-2');
+        button.innerHTML = `
+            <input type="checkbox" class="form-check-input me-2" name="subject" ${subject.name === 'All Subjects' ? 'checked' : ''}>
+            <span class="me-2"><i class="bi bi-${subject.icon}"></i></span>
+            ${subject.name}
+        `;
+
+        async function onclick() {
+            const checkbox = button.querySelector('input[type="checkbox"]');
+            const subjectName = subject.name.toLowerCase();
+
+            checkbox.checked = !checkbox.checked;
+
+            if (checkbox.checked) selectedSubjects.add(subjectName);
+            else selectedSubjects.delete(subjectName);
+
+            document.getElementById('posts-container').innerHTML = '';
+            currentPage = 1;
+            lastPostTimestamp = null;
+            reachedEnd = false;
+            await fetchPosts();
+        }
+
+        button.addEventListener('click', onclick);
+
+        buttonDiv.appendChild(button);
+        subjectButtonsContainer.appendChild(buttonDiv);
+    });
+}
