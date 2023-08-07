ccdb6318775ab8c23092ecc2343d3eecaa039fa3
TheTrustyPwo <shucecai@gmail.com>
2023-08-07 09:45:59 +0800

    Display post attachment errors

--- app/post/forms.py
+++ app/post/forms.py
@@ -7,4 +7,4 @@ class CreatePostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=1, max=80)])
     body = TextAreaField('Body', validators=[DataRequired(), Length(min=1, max=2000)])
     subject = SelectField('Subject', choices=[('english', 'English'), ('mathematics', 'Mathematics'), ('physics', 'Physics'), ('chemistry', 'Chemistry'), ('biology', 'Biology'), ('social_studies', 'Social Studies'), ('geography', 'Geography'), ('history', 'History'), ('chinese', 'Chinese'), ('computing', 'Computing'), ('literature', 'Literature'), ('music', 'Music'), ('art', 'Art')])
-    attachment = FileField('Attachment', validators=[FileAllowed(['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx'])])
+    attachment = FileField('Attachment', validators=[FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
