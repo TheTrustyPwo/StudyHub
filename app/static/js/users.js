7cccaca338f4f04d5d380a6317a5743b176dfc13
TheTrustyPwo <shucecai@gmail.com>
2023-08-07 15:08:40 +0800

    Revamp user page

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -1,19 +1,14 @@
 import { User, Conversation } from "./models/index.js";
 
-$(document).ready(async function () {
-    const username = window.location.pathname.split("/").pop();
-    const user = await User.getByUsername(username);
-    document.getElementById('username').innerText = user.username;
-    document.getElementById('datecreated').innerText = user.dateCreated;
-    document.getElementById('email').innerText = user.email;
-    const current = await User.getCurrent();
-    const message = document.getElementById('message');
-    message.style.visibility = 'hidden';
-    if (current.id !== user.id) {
-        message.style.visibility = 'visible';
-    }
+const username = window.location.pathname.split("/").pop();
+const user = await User.getByUsername(username);
 
-    message.onclick = async () => {
+$(document).ready(async function () {
+    document.getElementById('username').innerText = user.username;
+    document.getElementById('email').innerText = user.email;
+    document.getElementById('pfp').src = user.pfp;
+
+    document.getElementById('message').onclick = async () => {
         await Conversation.createPrivate(user.id);
         window.location.pathname = 'messages';
     };
